327c3f6656dcfb8b515684cf7e723eaca1720e17
afemz <andhika.favian18@gmail.com>
2024-01-17 16:50:55 +0700

    deleted unused code

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-# import PUMP_RESOLUTION_ROTATION
-# import PUMP_KETCHUP as PK
-# import PUMP_ROTATION as PR
 import time
 import sys
 
-path = "D:\Side Project\mietoma\Py\LIBRARY"
+path = "D:\Side Project\mietoma\Py\LIBRARY"  # Determine system path
 
 sys.path.append(path)
-
 
 from LIBRARY.PUMP_KETCHUP import PUMP_KETCHUP_FULL_ROTATION  # Buat manggil fungsi
 from LIBRARY.PUMP_RESOLUTION_ROTATION import FULL_RESOLUTION  # Buat manggil class
 
-
 PUMP_KETCHUP_FULL_ROTATION()
-
-# print("ini count" + count)
-
 
 """
 # Buat declare class
